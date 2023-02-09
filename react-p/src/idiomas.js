8d18025fb08b76559ecfbf6622633f76f4deca4a
330namajnoon <92964328+330namajnoon@users.noreply.github.com>
2023-02-09 13:15:51 +0100

    Add files via upload

---
+++ react-p/src/idiomas.js
@@ -0,0 +1,257 @@
+const idiomas = {
+    casa:{
+        espanol: "Inicio",
+        ingles: "Home",
+        turco:"Ev",
+    },
+    sobremi:{
+        espanol: "Sobre mi",
+        ingles: "About me",
+        turco:"Benim hakkımda"
+    },
+    proyectos:{
+        espanol: "Proyectos",
+        ingles: "Projects",
+        turco:"Projeler",
+    },
+    contacto:{
+        espanol:"Contacto",
+        ingles: "Contact",
+        turco:"İletişim",
+    },
+    herfe:{
+        espanol:"Programador web FULL STACK",
+        ingles: "FULL STACK WEB DEVELOPER",
+        turco:"Tam donanımlı web geliştiricisi",
+    },
+    chizikedustdaram: {
+        espanol: "Desarrollador front-end enfocado en la creación de aplicaciones web. Me gustan mucho los proyectos diferentes que resuelven problemas reales.",
+        ingles: "Front-end developer focused on creating Web applications. I really like different projects that solve real problems.",
+        turco:"Ön uç geliştirici, Web oluşturmaya odaklanmıştır. Gerçek sorunları çözen farklı projeleri gerçekten seviyorum.",
+    },
+    descargarcv:{
+        espanol:"Descargar CV",
+        ingles: "Download CV",
+        turco:"CV'yi indir",
+    },
+    darbareman: {
+        espanol: "Soy Sina Majnoon, tengo 22 años y vivo en Madrid. Soy un programador full-stack con habilidades en front-end y diseño de UI. Estoy apasionado por las interfaces, desde su creación hasta su desarrollo en el back-end. Me gusta aprender más y más sobre el área, los desafíos que me sacan de mi zona de confort son los mejores. He trabajado en el desarrollo de páginas de destino, sitios web, aplicaciones y programas completos.",
+        ingles: "I am Sina Majnoon, I am 22 years old and I live in Madrid. I am a full-stack programmer with front-end and UI design skills. I am passionate about interfaces, from its creation to its development in the back-end. I like to learn more and more about the area, challenges that take me out of my comfort zone are the best. I have worked on developing landing pages, websites, apps, and entire programs.",
+        turco:"Ben Sina Majnoon, 22 yaşındayım ve Madrid'de yaşıyorum. Ön uç geliştirici ve UI Tasarımcısı olarak, yaratılmalarından geliştirilmesine kadar arayüzler konusunda tutkuluyum. Alan hakkında gittikçe daha fazla şey öğrenmeyi seviyorum, beni konfor alanımdan çıkaran zorluklar en iyisidir. Açılış Sayfaları, Siteler, E-ticaret, E-posta Pazarlama, Uygulamalar ve Programların prototipini yaptım ve geliştirdim.",
+    },
+    habilidades: {
+        espanol: "Habilidades",
+        ingles:"Skills",
+        turco:"Yetenekler",
+    },
+    informacion: {
+        espanol: "Información",
+        ingles: "Information",
+        turco:"Bilgi",
+    },
+    datosacademicos: {
+        espanol: "Datos Academicos",
+        ingles: "Academic Data",
+        turco:"Akademik Veriler",
+    },
+    experiencias: {
+        espanol: "Experiencias",
+        ingles: "Experiences",
+        turco: "Deneyimler",
+    },
+    idiomas: {
+        espanol: "Idiomas",
+        ingles: "Languages",
+        turco: "Diller",
+    },
+    titulos:[{
+        nombre:{
+            espanol: "Programacion web full stack",
+            ingles: "Full stack web programming",
+            turco: "Tam yığın web programlama",
+        },
+        datos:{
+            espanol:["Fundamentos del desarrollo web","Javascript","Bases de datos","Frontend: ReacrJS","backend: NodeJS-PHP-JSP"],
+            ingles:["Fundamentals of web development","Javascript","Databases","Frontend: ReacrJS","backend: NodeJS-PHP-JSP"],
+            turco:["Web geliştirmenin temelleri","Javascript","Veritabanları","Ön uç: ReacrJS","arka uç: NodeJS-PHP-JSP"],
+        }
+    }],
+    trabajos:[
+        {
+            fecha:{
+                espanol: "Enero 2018 - Octubre 2021 (Turquia)",
+                ingles: "January 2018 - October 2021 (Turkey)",
+                turco: "Ocak 2018 - Ekim 2021 (Türkiye)",
+            },
+            nombre:{
+                espanol:"Programador de maquinas CNC",
+                ingles: "CNC machine programmer",
+                turco: "CNC makine programcısı",
+            },
+            datos:{
+                espanol:["Programacion  de maquinas CNC y mecanizado"],
+                ingles: ["Programming of CNC machines and machining"],
+                turco: ["CNC makinelerinin programlanması ve işlenmesi"],
+            }    
+        },
+        {
+            fecha:{
+                espanol: "Enero 2018 - Octubre 2021 (Turquia)",
+                ingles: "January 2018 - October 2021 (Turkey)",
+                turco: "Ocak 2018 - Ekim 2021 (Türkiye)",
+            },
+            nombre:{
+                espanol:"Diseñador de interiores",
+                ingles:"Interior designer",
+                turco:"iç mimar",
+            },
+            datos:{
+                espanol:["Dibujar planes de diseño ,Promocionar nuevos proyectos"],
+                ingles:["Draw design plans ,Promote new projects"],
+                turco:["Tasarım planları çizin, yeni projeleri teşvik edin"],
+            }
+        }
+    ],
+    zabanha:[
+        {
+            idioma:{
+                espanol:"Español",
+                ingles:"Spanish",
+                turco:"İspanyolca",
+            },
+            nivel:{
+                espanol:"B2",
+                ingles:"B2",
+                turco:"B2",
+            }
+        },
+        {
+            idioma:{
+                espanol:"Turco",
+                ingles:"Turkish",
+                turco:"Türkçe",
+            },
+            nivel:{
+                espanol:"C1",
+                ingles:"C1",
+                turco:"C1",
+            }
+        },
+        {
+            idioma:{
+                espanol:"Persa",
+                ingles:"Persian",
+                turco:"Farsça",
+            },
+            nivel:{
+                espanol:"Nativa",
+                ingles:"Native",
+                turco:"Yerli",
+            }
+        }
+    ],
+    projeha:[
+        {
+            nombre:{
+                espanol:"Juego de kartas",
+                ingles:"Card Game",
+                turco:"Kartlar oyunu",
+            },
+            datos:"Javascript-nodeJS-HTML-CSS",
+            visitar:[{name:"",link:"https://shadowed-ionized-capri.glitch.me/"}],
+            discripcion:{
+                espanol:"es un juego en línea emocionante para 2 o más jugadores. Para comenzar, los jugadores deben registrarse por primera vez, eligiendo un nombre de usuario y una foto. Luego, pueden invitar a otros jugadores en línea para unirse a la diversión. El objetivo del juego es tener la suma de 11 con tus cartas y además hay tres cartas especiales, como la carta J, que puede recoger todas las cartas excepto las cartas K y Q, y la carta Q solo puede recoger la Q, mientras que la carta K solo puede recoger la K. Cada jugador comienza con cuatro cartas, que solo pueden ver, y cuatro cartas en la mesa. A medida que avanza el juego, se reparten nuevas cartas y se lleva a cabo un seguimiento de los puntos hasta que se terminen las 52 cartas.",
+                ingles: "is an exciting online game for 2 or more players. To get started, players must register for the first time, choosing a username and photo. They can then invite other online players to join the fun. The object of the game is to have the sum of 11 with your cards and in addition there are three special cards, such as the J card, which can collect all cards except the K and Q cards, and the Q card. can only pick up the Q, while the K card can only pick up the K. Each player starts with four cards, which they can only see, and four cards on the table. As the game progresses, they are new cards are dealt and points are tracked until all 52 cards are used up.",
+            },
+            codigo:"https://glitch.com/edit/#!/shadowed-ionized-capri?path=server.js%3A19%3A1",
+            image:"juegodecartas.png",
+
+        },
+        {
+            nombre:{
+                espanol:"Modelo 3D",
+                ingles:"3D Model",
+                turco:"3 boyutlu model",
+            },
+            datos:"Javascript-nodeJS-ThreeJS",
+            visitar:[{name:"",link:"https://kiwi-chill-author.glitch.me/"}],
+            discripcion:{
+                espanol:"es una experiencia única en línea que combina las últimas tecnologías de Three.js y WebGL para crear un objeto 3D interactivo. Al pulsar en el objeto, los usuarios pueden acceder a una opción que les permite cambiar sus colores con un solo clic. Además, al pulsar en un cajón, este se abre y cierra con un efecto suave y sorprendente.",
+                ingles:"is a unique online experience that combines the latest Three.js and WebGL technologies to create an interactive 3D object. By clicking on the object, users can access an option that allows them to change its colors with a single click"
+            },
+            codigo:"https://glitch.com/edit/#!/kiwi-chill-author",
+            image:"modulo3d.png",
+        },
+        {
+            nombre:{
+                espanol:"Aplicacion de Examenes",
+                ingles:"exam application",
+                turco:"sınav uygulaması",
+            },
+            datos:"Javascript-nodeJS-HTML-CSS",
+            visitar:[{name:"Estudiante",link:"https://fluttering-miniature-hide.glitch.me"},{name:"Profesor",link:"https://fluttering-miniature-hide.glitch.me/admin"}],
+            discripcion:{
+                espanol:"es una aplicación web revolucionaria para la creación y realización de exámenes. Con dos enlaces claramente definidos, los profesores pueden agregar preguntas o iniciar un examen, mientras que los estudiantes pueden acceder y realizar el examen una vez que el profesor dé el visto bueno.",
+                ingles:"is a revolutionary web application for creating and taking exams. With two clearly defined links, teachers can add questions or start a test, while students can access and take the test once the teacher gives the go-ahead."
+            },
+            codigo:"https://glitch.com/edit/#!/fluttering-miniature-hide",
+            image:"examen.png",
+        },
+        {
+            nombre:{
+                espanol:"Portafolio",
+                ingles:"Ptortfolio",
+                turco:"Portfolyo",
+            },
+            datos:"Javascript-nodeJS-reactJS-HTML-CSS",
+            visitar:[{name:"",link:"https://cooing-impartial-millennium.glitch.me"}],
+            discripcion:{
+                espanol:"es una plataforma web dinámica y moderna construida con React JS. Aquí, los visitantes pueden obtener una visión completa de mis proyectos y habilidades, gracias a una interfaz atractiva y fácil de usar. Desde mis proyectos más recientes hasta mis habilidades y logros, todo está disponible en un solo lugar.",
+                ingles:"is a dynamic and modern web platform built with React JS. Here, visitors can get a complete overview of my projects and skills, thanks to an attractive and easy-to-use interface. From my most recent projects to my skills and achievements, it's all available in one place."
+            },
+            codigo:"https://glitch.com/edit/#!/cooing-impartial-millennium",
+            image:"miportafolio.png",
+        },
+    ],
+    codigo:{
+        espanol:"Codigo",
+        ingles:"Code",
+        turco:"kod",
+    },
+    visitar:{
+        espanol:"Visitar",
+        ingles:"to visit",
+        turco:"ziyaret et",
+    },
+    nombre:{
+        espanol:"Tu nombre",
+        ingles: "Your name",
+        turco: "Adınız",
+    },
+    email:{
+        espanol:"Tu email",
+        ingles: "Your email",
+        turco: "E-posta adresiniz",
+    },
+    asunto:{
+        espanol:"Tu asunto",
+        ingles:"Your subject",
+        turco:"konunuz",
+    },
+    mensaje: {
+        espanol:"Mensaje",
+        ingles:"Message",
+        turco:"Mesajiniz",
+    },
+    enviar:{
+        espanol:"Enviar",
+        ingles:"Send",
+        turco:"Gönder",
+    }
+
+
+
+
+}
+
+export default idiomas;
